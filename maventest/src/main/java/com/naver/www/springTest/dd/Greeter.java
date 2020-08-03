6743c10651a1cfdeafa6777ea25b5779ae8b753f
seiren <51488875+seirenz@users.noreply.github.com>
2020-08-03 17:04:39 +0900

    greeter, xml

--- maventest/src/main/java/com/naver/www/springTest/dd/Greeter.java
+++ maventest/src/main/java/com/naver/www/springTest/dd/Greeter.java
@@ -1,5 +1,19 @@
 package com.naver.www.springTest.dd;
 
 public class Greeter {
+	private String format;
+	
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+	
+	public String greet(String guest) {
+		return String.format(format, guest);
+	}
 
 }
--- maventest/src/main/java/com/naver/www/springTest/dd/Main.java
+++ maventest/src/main/java/com/naver/www/springTest/dd/Main.java
@@ -1,5 +1,11 @@
 package com.naver.www.springTest.dd;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class Main {
+	public static void main(String [] args) {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		Greeter gr  = ctx.getBean("gr", Greeter.class);
+	}
 
 }
